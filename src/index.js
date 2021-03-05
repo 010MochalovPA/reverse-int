6766f831b172f3b111b3374d17cd6f2e5fa6e5f4
Павел Мочалов <78229095+010MochalovPA@users.noreply.github.com>
2021-03-05 23:11:33 +0300

    feat: add reverse Int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    return +(String(Math.abs(n)).split('').reverse().join(''));
+    // return  Number((String(Math.abs(n))).split('').reverse().join(''));
 }
